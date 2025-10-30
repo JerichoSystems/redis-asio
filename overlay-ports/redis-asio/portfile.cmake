341fcfca873f22596f9553c8754f825262921de3
Pieter Willem Jordaan <pieter@jerichosystems.co.za>
2025-10-30 09:51:05 +0200

    chore(version): Bump to version 0.1.9

--- overlay-ports/redis-asio/portfile.cmake
+++ overlay-ports/redis-asio/portfile.cmake
@@ -14,7 +14,7 @@ else()
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO jerichosystems/redis-asio
-        REF v0.1.8
+        REF v0.1.9
         SHA512 0 # fill when publishing a tag
     )
 endif()
